ae64a438ba35a1245ac95ba83b1a51b36dc719ab
Zbigniew Cebula <rrrafalon@gmail.com>
2015-11-03 23:30:05 +0100

    Update Snake'a - Podział na podklasy dla uporządkowania kodu, rozbicie na klasę gry i klasę menu. (w drodzę klasa animacji)

---
+++ SHTerminal/Assets/Scripts/SHGUI/OnionMilk/Snake/APPSnake.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+using System.Collections;
+
+public class APPSnake : SHGUIappbase {
+	//Private
+	APPSnakeGame	gameHandle	= null;
+	APPSnakeMenu	menuHandle	= null;
+	
+	bool			menuPhase	= true;
+	bool			gamePhase	= false;
+	//Public
+	public APPSnake()
+		:	base("snake-vS.S.S.BOOM-by-onionmilk")
+	{
+		gameHandle	= new APPSnakeGame();
+		menuHandle	= new APPSnakeMenu();
+	} 
+
+	public override void Update() {
+		base.Update();
+		if (gamePhase)
+			gameHandle.Update();
+		else if (menuPhase)
+			menuHandle.Update();
+		//--
+	}
+
+	public override void Redraw(int offx, int offy) {
+		base.Redraw(offx, offy);
+		//SHGUI.current.SetPixelFront(char character, int posX, int posY, char color);
+
+		if (menuPhase) {
+			//Menu
+			int	offset	= 0;
+			for(int x = 0; x < menuHandle.menuOptionsBar.Length; ++x) {
+				offset	 = (x + menuHandle.optionsOffset)%menuHandle.menuOptionsBar.Length;
+				if (offset < 0)
+					offset += menuHandle.menuOptionsBar.Length;
+				//--
+				SHGUI.current.SetPixelFront(
+					menuHandle.menuOptionsBar[offset],
+					2 + x,
+					1 + y,
+					'w'
+				);
+			}
+			
+		} else if (gamePhase) {
+			if (gameHandle.snakeDead)	return;
+
+			//Map
+			for(int y = 0; y < 22; ++y)
+				for(int x = 0; x < gameHandle.map.GetLength(0); ++x) {
+					//Ściana
+					if (gameHandle.map[x, (y * 2)] == 1 && gameHandle.map[x, (y * 2) + 1] == 1) {
+						SHGUI.current.SetPixelBack(gameHandle.objList[3], 1 + x, 1 + y, 'w');
+					} else if (gameHandle.map[x, (y * 2)] == 1 && gameHandle.map[x, (y * 2) + 1] != 1) {
+						SHGUI.current.SetPixelBack(gameHandle.objList[1], 1 + x, 1 + y, 'w');
+					} else if (gameHandle.map[x, (y * 2)] != 1 && gameHandle.map[x, (y * 2) + 1] == 1) {
+						SHGUI.current.SetPixelBack(gameHandle.objList[2], 1 + x, 1 + y, 'w');
+					}
+					
+					//Jedzenie
+					if (gameHandle.map[x, (y * 2)] == 2 && gameHandle.map[x, (y * 2) + 1] == 2) {
+						SHGUI.current.SetPixelBack(gameHandle.objList[6], 1 + x, 1 + y, 'r');
+					} else if (gameHandle.map[x, (y * 2)] == 2 && gameHandle.map[x, (y * 2) + 1] != 2) {
+						SHGUI.current.SetPixelBack(gameHandle.objList[4], 1 + x, 1 + y, 'r');
+					} else if (gameHandle.map[x, (y * 2)] != 2 && gameHandle.map[x, (y * 2) + 1] == 2) {
+						SHGUI.current.SetPixelBack(gameHandle.objList[5], 1 + x, 1 + y, 'r');
+					}
+				}
+			//--
+			//Snake
+			for(int y = 0; y < 22; ++y)
+				for(int x = 0; x < gameHandle.map.GetLength(0); ++x)
+					if (gameHandle.snakeTailMap[x, (y * 2)] > 0 && gameHandle.snakeTailMap[x, (y * 2) + 1] > 0) {
+						SHGUI.current.SetPixelFront(gameHandle.snakeGfx[2], 1 + x, 1 + y, gameHandle.snakeColor);
+					} else if (gameHandle.snakeTailMap[x, (y * 2)] > 0 && gameHandle.snakeTailMap[x, (y * 2) + 1] <= 0) {
+						SHGUI.current.SetPixelFront(gameHandle.snakeGfx[0], 1 + x, 1 + y, gameHandle.snakeColor);
+					} else if (gameHandle.snakeTailMap[x, (y * 2)] <= 0 && gameHandle.snakeTailMap[x, (y * 2) + 1] > 0) {
+						SHGUI.current.SetPixelFront(gameHandle.snakeGfx[1], 1 + x, 1 + y, gameHandle.snakeColor);
+					}
+				//--
+			//--
+		}
+	}
+
+	public override void ReactToInputKeyboard(SHGUIinput key) {
+		if (gamePhase) {
+			if (gameHandle.InputUpdate(key) == false) {
+				SHGUI.current.PopView();
+			}
+		} else if (menuPhase)
+			if (menuHandle.InputUpdate(key) == false) {
+				SHGUI.current.PopView();
+			}
+		//--
+	}
+
+	
+}
---
+++ SHTerminal/Assets/Scripts/SHGUI/OnionMilk/Snake/APPSnakeMenu.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class APPSnakeMenu {
+	//Public
+	
+	public string	menuOptionsBar		= "    [ CLASSIC ]       [ WITH BONUSES ]        [ EXIT ]      ";
+	public int		currentOption		= 0;
+	public int		optionsOffset		= 40;
+	public float	optionsOffsetTarget	= 40;
+	public float	optionsAnimTimer	= 0;
+
+	public APPSnakeMenu() {
+
+	}
+
+	public void Update() {
+		optionsAnimTimer	+= Time.unscaledDeltaTime;
+		if (optionsOffset != optionsOffsetTarget
+		&&	optionsAnimTimer > 0.025f
+		) {
+			if (optionsOffset >= optionsOffsetTarget) {
+				optionsOffset		= (optionsOffset + 1)%menuOptionsBar.Length;
+				--optionsOffsetTarget;
+			} else {
+				optionsOffset		= (optionsOffset - 1)%menuOptionsBar.Length;
+				++optionsOffsetTarget;
+			}//Licznik przesunięć
+			
+			optionsAnimTimer	= 0;
+		}
+	}
+
+	public bool InputUpdate(SHGUIinput key) {
+		if (optionsOffset == optionsOffsetTarget) {
+			if (key == SHGUIinput.right || key == SHGUIinput.down)
+				optionsOffsetTarget	= optionsOffset + 20;
+			//--
+			if (key == SHGUIinput.left || key == SHGUIinput.up)
+				optionsOffsetTarget	= optionsOffset - 20;
+			//--
+		}
+
+		if (key == SHGUIinput.esc)
+			return false;
+		//--
+		
+		return true;
+	}
+}
